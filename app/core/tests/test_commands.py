f309fd3f0cfcc6c3089d8cad7dfff4567ff615c8
BhargavaGY <bhargava.cs20@bmsce.ac.in>
2024-07-10 15:17:50 +0530

    Added wait for database functionality

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,36 @@
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+# Psycopg2Error when db is not ready to accept connections
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+# OperationalError when test db is not yet created but it is ready for connection
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for db if db is ready"""
+        # mocking db state of ready
+        patched_check.return_value = True
+
+        # executing wait_for_db module
+        call_command('wait_for_db')
+
+        # making sure that check is called with correct db
+        patched_check.assert_called_once_with(databases=['default'])
+    
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting Operational Errors"""
+        # mocking db various db states
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
+        
