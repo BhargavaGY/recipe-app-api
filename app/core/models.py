c21a49ee6dbf1c84fccd668991df45aba8f7f05f
BhargavaGY <bhargava.cs20@bmsce.ac.in>
2024-07-10 21:45:23 +0530

    Added User model and corresponding test scripts

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,30 @@
-from django.db import models   # noqa
+"""
+Models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """Manager for user"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User Model"""
+
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
